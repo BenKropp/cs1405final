ef0b763fc7ef6cb3985e18108edbccfe9f96da6d
Ben Kropp <benkropp94@gmail.com>
2023-04-18 11:56:42 -0600

    got the validation for accessing directories working

--- Program.cs
+++ Program.cs
@@ -3,8 +3,18 @@ Console.Clear();
 static void AccessDB(string path){
 
 }
+static bool Validate(string test, int mode){
+    if(Path.Exists(test) && mode == 1){
+        return true;
+    } else if (!Path.Exists(test) && mode == 1){
+        Console.WriteLine("Please enter a valid file path");
+        Validate(Console.ReadLine(), 1);
+        return false;
+    } else return false;
+}
 Console.WriteLine(@"Welcome to the UDT (Unnamed Database Tool)!
 Press any key to continue");
 Console.ReadKey(true);
 Console.WriteLine("Please enter the file location where you would like to access your database from");
 string? path = Console.ReadLine();
+Validate(path, 1);
