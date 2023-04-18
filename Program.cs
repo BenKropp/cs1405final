3a58e50ffb61f1c68df40d1221269d445412552c
Ben Kropp <benkropp94@gmail.com>
2023-04-17 20:58:23 -0600

    initial commit, added basic print statements

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Final Project Database Tool Ben Kropp
+Console.Clear();
+static void AccessDB(string path){
+
+}
+Console.WriteLine(@"Welcome to the UDT (Unnamed Database Tool)!
+Press any key to continue");
+Console.ReadKey(true);
+Console.WriteLine("Please enter the file location where you would like to access your database from");
+string? path = Console.ReadLine();
